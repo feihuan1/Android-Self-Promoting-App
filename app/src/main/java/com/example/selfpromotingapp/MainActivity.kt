43741d87d970c8f118206c5f90a93ca91201d27f
Feihuan Peng <feihuan.peng@spreetail.com>
2025-09-28 23:27:47 -0500

    asd

--- app/src/main/java/com/example/selfpromotingapp/MainActivity.kt
+++ app/src/main/java/com/example/selfpromotingapp/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.example.selfpromotingapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val previewButton: Button = findViewById(R.id.button_preview)
+        previewButton.setOnClickListener {
+            onPreviewClicked()
+        }
+    }
+
+    private fun onPreviewClicked() {
+        val contactNameEditText: TextInputEditText = findViewById(R.id.edit_text_contact_name)
+        val contactNumberEditText: TextInputEditText = findViewById(R.id.edit_text_contact_number)
+        val textString = contactNameEditText.text.toString() + ", " + contactNumberEditText.text.toString()
+
+        Toast.makeText(this, textString, Toast.LENGTH_LONG).show()
     }
 
 }
