d9954a9484350f7521865b1ae49138561f60dbfb
Feihuan Peng <feihuan.peng@spreetail.com>
2025-10-03 00:48:23 -0500

    asd

--- app/src/main/java/com/example/selfpromotingapp/MainActivity.kt
+++ app/src/main/java/com/example/selfpromotingapp/MainActivity.kt
@@ -1,27 +1,43 @@
 package com.example.selfpromotingapp
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
+    private var contactNameEditText: TextInputEditText? = null
+    private var contactNumberEditText: TextInputEditText? = null
+    private var myDisplayNameEditText: TextInputEditText? = null
+    private var startDateEditText: TextInputEditText? = null
+    private var juniorCheckBox: CheckBox? = null
+    private var immediateStartCheckBox: CheckBox? = null
+    private var jobTitleSpinner: Spinner? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        contactNameEditText = findViewById(R.id.edit_text_contact_name)
+        contactNumberEditText = findViewById(R.id.edit_text_contact_number)
+        myDisplayNameEditText = findViewById(R.id.edit_text_my_display_name)
+        startDateEditText = findViewById(R.id.edit_text_start_date)
+        juniorCheckBox = findViewById(R.id.check_box_junior)
+        immediateStartCheckBox = findViewById(R.id.check_box_immediate_start)
+        jobTitleSpinner = findViewById(R.id.spinner_job_title)
+
         val previewButton: Button = findViewById(R.id.button_preview)
         previewButton.setOnClickListener {
             onPreviewClicked()
         }
     }
-
     private fun onPreviewClicked() {
-        val contactNameEditText: TextInputEditText = findViewById(R.id.edit_text_contact_name)
-        val contactNumberEditText: TextInputEditText = findViewById(R.id.edit_text_contact_number)
-        val textString = contactNameEditText.text.toString() + ", " + contactNumberEditText.text.toString()
-
+        val textString = contactNameEditText?.text.toString() + ", " + contactNumberEditText!!.text.toString()
         Toast.makeText(this, textString, Toast.LENGTH_LONG).show()
     }
 
